227bcf16720dd66045be6513f3925086280ceebc
Maria Hildebrandt <hildebrandt.maria@gmail.com>
2019-11-30 00:31:15 +0100

    commit before new branch

--- client/src/pages/projects/index.js
+++ client/src/pages/projects/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Header, Segment } from 'semantic-ui-react'
 
 import Create from './Create';
 import List from './List';
@@ -9,12 +10,17 @@ import Details from './Details';
 const Projects = ({ match }) => {
   return (
     <>
+    <Header as='h1' textAlign='center'>
+        Tasks
+      </Header>
+      <Segment>
         <Switch>
           <Route exact path={match.path} component={List} />
           <Route path={`${match.path}/create`} component={Create} />
           <Route path={`${match.path}/details/:_id`} component={Details} />
           <Route path={`${match.path}/:_id`} component={Update} />
         </Switch>
+      </Segment>
     </>
   )
 }
